07a3eb8eb5965d3bd1566019fbd5fc89b560be5e
Alan Sarkar <55506052+alansarkar@users.noreply.github.com>
2020-07-08 23:42:59 +0000

    Update yt.sh

--- yt.sh
+++ yt.sh
@@ -6,19 +6,20 @@ color4='tput setaf 11' #yelow
 color5='tput setaf 1'  #red
 color6='tput setaf 14'  
 
+
+####### ###     edit the video player from here         ################
+player="/usr/bin/mpv --really-quiet  "
+
+
 sandbox='firejail' 
 #$names='| grep '{"accessibilityData":{"label":"' -F  | sed 's/"}],"accessibility":{"accessibilityData":{"label":"/\n/g' | cut -d' ' -f1-10  | tail -n +2 '
 #$links='| grep '","webPageType' \| sed 's/\",\"webPageType/\n/g'  | grep watch?v | sed 's/.*\/watch?v=//g' | cut -d' ' -f1 '
-
-########### change the video player from here  ##########
-player=/usr/bin/mpv
 
 tag="$@"
 rm -rf /tmp/.ytcache
 /bin/clear
 re=1
 #redo
-
 
 while [  $re != q  ]
 do
@@ -38,11 +39,6 @@ clear
 #echo "$($sandbox /usr/bin/wget -qO-  https://www.youtube.com/results?search_query="$x"&spfreeload=10)"      | grep '<a href="/watch?v=' | grep -v  '<li><div class="yt-lockup yt-lockup-tile yt-lockup-play    list vve-check clearfix"' | grep -v '<li class="yt-lockup-playlist-item clearfix"><span class="    yt-lockup-playlist-item-length">' > /tmp/.ytcache
 
 echo "$($sandbox /usr/bin/wget -qO-  https://www.youtube.com/results?search_query="$x"&spfreeload=10)" > /tmp/.ytcache 
-## name
-cat /tmp/.ytcache   | grep '{"accessibilityData":{"label":"' -F  | sed 's/"}],"accessibility":{"accessibilityData":{"label":"/\n/g' | cut -d' ' -f1-10  | tail -n +2   > /tmp/.ytname
-## link
-cat /tmp/.ytcache  | grep '","webPageType' | sed 's/\",\"webPageType/\n/g'  | grep watch?v | sed 's/.*\/watch?v=//g' | cut -d' ' -f1   > /tmp/.ytlink
-
 
 
 #re view search result
@@ -50,6 +46,11 @@ re2=1
 while [ $re2 -eq 1 ]
 do
 y=1
+## name
+cat /tmp/.ytcache   | grep '{"accessibilityData":{"label":"' -F  | sed 's/"}],"accessibility":{"accessibilityData":{"label":"/\n/g' | cut -d' ' -f1-10  | tail -n +2   > /tmp/.ytname
+## link
+cat /tmp/.ytcache  | grep '","webPageType' | sed 's/\",\"webPageType/\n/g'  | grep watch?v | sed 's/.*\/watch?v=//g' | cut -d' ' -f1   > /tmp/.ytlink
+
 
 z=$(cat /tmp/.ytname | wc -l ) 
 
@@ -109,7 +110,7 @@ echo ""
 #"$sandbox" --quiet  --private --disable-mnt --noexec=all --nonewprivs --noroot mpv --brightness 7 --geometry 800x450 --quiet   $tag  --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.129 Safari/537.36" --ytdl-format=best  "https://www.youtube.com/watch?v=$q"
 #### ffplay 
 #"$sandbox"  --private --noroot --quiet youtube-dl  --user-agent "fucku" -f best -c -q  "https://www.youtube.com/watch?v=$q" -o - |   ffplay -loglevel quiet  -
-"$sandbox"  --private --noroot --quiet /usr/bin/youtube-dl  -q --user-agent "fucku"  -c  "https://www.youtube.com/watch?v=$q" -o - |    $player    --really-quiet    -
+"$sandbox"  --private --noroot --quiet /usr/bin/youtube-dl  -q --user-agent "fucku"  -c  "https://www.youtube.com/watch?v=$q" -o - |   $player    -
 #/bin/sh /home/$(whoami)/my\ scripts/mpv.sh "https://www.youtube.com/watch?v=$q" 
 
 mpv=1 # for conflict
@@ -165,7 +166,10 @@ fi # end of $p = n
 if  (([ "$p" != n ] && [ "$p" != q  ]) && [ "$mpv" != 1 ])
 then
  
-echo "$("$sandbox" /usr/bin/wget -qO-  https://www.youtube.com/results?search_query="$p"&spfreeload=10)"      | grep '<a href="/watch?v=' | grep -v  '<li><div class="yt-lockup yt-lockup-tile yt-lockup-play    list vve-check clearfix"' | grep -v '<li class="yt-lockup-playlist-item clearfix"><span class="    yt-lockup-playlist-item-length">' > /tmp/.ytcache
+rm -rf /tmp/.ytlink
+rm -rf /tmp/.ytname
+rm -rf /tmp/.ytcache
+echo "$("$sandbox" /usr/bin/wget -qO-  https://www.youtube.com/results?search_query="$p"&spfreeload=10)"      > /tmp/.ytcache
 x="$p"
 fi
 
