3d4d4b8e89d029b91ca7e52609797374ec940551
Alan Sarkar <55506052+alansarkar@users.noreply.github.com>
2020-09-26 15:30:26 +0000

    Update yt.sh

--- yt.sh
+++ yt.sh
@@ -1,16 +1,60 @@
 #!/bin/env bash 
 umask 0034
-set bell-style none
-set input-meta off
 unalias -a 
 unset  $GROUPS
 unset PATH
 export PATH=$PATH:/usr/bin/:~/.local/bin:~/my\ scripts
+rm -rf $imputdir 
 
-scraper=/usr/bin/wget  
-scrap_flag='--user-agent="$useragent"  -qO -   '
+player="/usr/bin/mpv   --really-quiet " 
+imageviewer="tiv" #-h 40 -w 40  "
 
+sandbox=/opt/firejail/bin/firejail 
+f='--noroot --private-cache  --quiet  --noroot --nonewprivs    --seccomp  '   ## firejail config for this script
+sandbox_flag="$sandbox $f " 
+scraper_flag="$scraper $scrap_flag"
 
+scraper="/bin/wget"
+scrap_flag='--user-agent="$useragent"  -qO -   '  #scrap_flag=' -s  -H "User-Agent: $useragent" -A "$useragent" '
+
+useragent="`random.exe`"                         #useragent="Mozilla/5.0 (X11; Linux x86_64; rv:52.9) Gecko/20100101 Firefox/52.9 (Pale Moon)"
+
+searchlink="https://youtube.com/results?search_query="
+watchlink="https://www.youtube.com/watch?v="
+
+#####################################################################################  
+case "$@" in
+    --help|-h) 
+echo "Usage:  yt.sh 
+    
+    Youtube Viewer 
+
+    Just execute this script and Enter search terms at prompt 
+    And select the number to play when the search results shown
+
+Options: 
+
+    --help                  Show this message and exit
+    --history               Shows previous search history
+    --clear                 clears search history
+    --image                 enables image
+" 
+      ;;
+  --history)
+if      [ -d ~/.config/youtube\ bash\ script ] &&  [ -f  ~/.config/youtube\ bash\ script/history ]  ; then 
+    cat ~/.config/"youtube bash script/history"
+else  
+    echo " No history to show "
+    fi
+      ;;  
+  --clear)
+if      [ -f  ~/.config/youtube\ bash\ script/history ]  ; then 
+    rm  ~/.config/"youtube bash script/history"
+    fi
+      ;;  
+
+  ""|--image)
+###########################     variables  ##################
 color1="tput setaf 15" #white
 color2='tput setaf 05' #pink
 color3='tput setaf 6'  #blue 
@@ -18,38 +62,21 @@ color4='tput setaf 11' #yelow
 color5='tput setaf 1'  #red
 color6='tput setaf 14'  
 
-
-imageenable=0  ##  0 disable 1 to enable 
-#sandbox=/bin/firejail 
-#f='--noroot --private-cache  --quiet  --noroot --nonewprivs    --seccomp  '
-
-sandbox_flag="$sandbox $f " 
-scraper_flag="$scraper $scrap_flag"
-
-#useragent="Mozilla/5.0 (X11; Linux x86_64; rv:52.9) Gecko/20100101 Firefox/52.9 (Pale Moon)"
-useragent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15"
-
-
-#$names='| grep '{"accessibilityData":{"label":"' -F  | sed 's/"}],"accessibility":{"accessibilityData":{"label":"/\n/g' | cut -d' ' -f1-10  | tail -n +2 '
-#$links='| grep '","webPageType' \| sed 's/\",\"webPageType/\n/g'  | grep watch?v | sed 's/.*\/watch?v=//g' | cut -d' ' -f1 '
-#$image='| sed 's/thumb/\n/g'  | grep jpg | cut -c 17-64 | uniq -u | grep http |sed 's/jpg.*//g''
-
+ ##  0 disable 1 to enable 
+if [ "$1"  == "--image" ] ;  then imageenable=1 ; else   imageenable=0;  fi  
 #### image folder #####
 imputdir=$HOME/.ytimg
 imputfile=$HOME/.ytimgs
-
-tag="$@"
-rm -rf $imputdir 
 cleaning_cache
 clear
 re=1
 #redo
-player="/usr/bin/mpv   --really-quiet " 
 
-imageviewer="tiv"
-#-h 40 -w 40  "
+###################################################
 
 
+
+########### function  #############
 
 function parsing() 
 {
@@ -57,6 +84,7 @@ function parsing()
 cat /tmp/.ytcache   | grep '{"accessibilityData":{"label":"' -F  | sed 's/"}],"accessibility":{"accessibilityData":{"label":"/\n/g' | cut -d' ' -f1-10  | tail -n +2   > /tmp/.ytname
 ## link
 cat /tmp/.ytcache  | grep '","webPageType' | sed 's/\",\"webPageType/\n/g'  | grep watch?v | sed 's/.*\/watch?v=//g' | cut -d' ' -f1   > /tmp/.ytlink
+
 }
 
 function clearing_cache()
@@ -66,10 +94,15 @@ rm -rf /tmp/.ytlink
 rm -rf /tmp/.ytname
 rm -rf $imputfile
 }
+function startup_name() {
+echo $($color1)  "*********youtube script **********$(tput sgr 0)"
+echo $($color2)  "                 by alan sarkar$(tput sgr 0)"
+echo $($color1)  "*********************************$(tput sgr 0)"
+echo $($color3)  " Enter what you want to search:$(tput sgr 0)"
+}
 
 function image_parsing()
 {
-#cat $imputfile  | head -$y | tail -1 
 if [ -d $imputdir  ] 
 then
     ypeg=$(expr $y - 1 ) 
@@ -78,29 +111,32 @@ else $imageviewer "$imputdir/$(cat $imputfile | cut -c 36-50  | head -$y | tail 
 fi
 }
 
+function history() {
+[ ! -d  $HOME/.config/"youtube bash script" ]  && touch $HOME/.config/"youtube bash script" ;
+
+echo "$@" >> $HOME/.config/"youtube bash script/history"
+ 
+
+}
+
+#########################
 
 while [  $re != q  ]
 do
-echo $($color1)  "*********youtube script **********$(tput sgr 0)"
-echo $($color2)  "                 by alan sarkar$(tput sgr 0)"
-echo $($color1)  "*********************************$(tput sgr 0)"
-echo $($color3)  " Enter what you want to search:$(tput sgr 0)"
+startup_name
 read input1 ;
 x="$input1"
 clear
 
-
-
 ##########   pulling name ######   
 xyz="`echo "$x" | sed 's/ /+/g'`"
-echo "$( $sandbox_flag   /usr/bin/wget --user-agent="$useragent" -qO-  https://www.youtube.com/results?search_query="$xyz"&spfreeload=10)" > /tmp/.ytcache 
+echo "$( $sandbox_flag  $scraper_flag  "$searchlink"$xyz"&spfreeload=10")" > /tmp/.ytcache 
+
 #### imagescrape 
 #if [ $imageenable  -eq 1 ] ;  then mkdir $imputdir ; fi 
 cat /tmp/.ytcache | sed 's/thumb/\n/g'  | grep jpg | cut -c 17-64 | uniq -u | grep http   |sed 's/jpg.*/jpg/g' > "$imputfile"
 if [ $imageenable  -eq 1 ] ; then $sandbox_flag  /usr/bin/wget --user-agent="$useragent" -q   --input-file=$imputfile -P  $imputdir/  ;  fi 
 
-
-#re view search result
 re2=1
 
 while [ $re2 -eq 1 ]
@@ -114,7 +150,7 @@ do
 printf  " $($color3) $y. "
 ## echo  name
 echo $($color4)  "$(cat /tmp/.ytname  |head -$y | tail -1)$(tput sgr 0)"
-#echo link : 
+## echo link : 
 cat /tmp/.ytlink | head -$y | tail -1
 
 image_parsing
@@ -137,50 +173,42 @@ then
 q=$(cat /tmp/.ytlink | head -$p | tail -1 )
 clear
 
+show_title="`cat /tmp/.ytname | head -$p | tail -1`"
 echo $($color1)  " Now Playing: "
 echo " "
-echo $($color4)  "$(cat /tmp/.ytname | head -$p | tail -1)$(tput sgr 0)"
-echo $($color4)  "$(echo "Link: https://www.youtube.com/watch?v=$q")$(tput sgr 0)"
+echo $($color4)  "$show_title$(tput sgr 0)"
+echo $($color4)  "$(echo "Link: "$watchlink"$q")$(tput sgr 0)"
 
 echo " "
 echo $($color1)  " Description: $(tput sgr 0)"
 
 
-
-
-
-
 ##################        discreaption ######################
 
-echo $($color6)  "$( $sandbox_flag   /usr/bin/wget  --user-agent="$useragent"  -qO-  "https://www.youtube.com/watch?v=$q" |  grep '},\"description\":{\"simpleText\":\"'  | sed 's/.*},\"description\":{\"simpleText\":\"//g;s/"},"lengthSeconds":".*//g;s/\\n/\n/g'   )$(tput sgr 0)"
+echo $($color6)  "$( $sandbox_flag  $scraper_flag  "$watchlink$q" |  grep '},\"description\":{\"simpleText\":\"'  | sed 's/.*},\"description\":{\"simpleText\":\"//g;s/"},"lengthSeconds":".*//g;s/\\n/\n/g'   )$(tput sgr 0)"
 #fi
 echo "" 
 
 
 #####  video play ###########
 
-#$HOME/my\ scripts/mpdl.sh "https://www.youtube.com/watch?v=$q"
+$HOME/my\ scripts/mpdl.sh "$watchlink$q"
 
-$sandbox  youtube-dl  -q --user-agent "$useragent"  -c  "https://www.youtube.com/watch?v=$q" -o - |   $player   -
-
-
+#$sandbox  youtube-dl  -q --user-agent "$useragent"  -c  "$watchlink$q" -o - |   $player   -
+history "$show_title: $watchlink$q "
 mpv=1 # for conflict
 #clear
 
 fi
-
-
+########################
+## exit ##
 if [ "$p" = q ] 
 then
 clear
 
-
 re2=0
-#re=q   # complete exit var
 clearing_cache
 fi
-
-
 
 ######## next page   ######
 if [ "$p" = n ] 
@@ -193,48 +221,45 @@ clear
 
 if [ "$xx" != 1 ] && [[ "$xx" =~ ^[0-9]+$ ]] 
 then
-#### page number main search ####
 xyz="`echo "$x" | sed 's/ /+/g'`"
-echo "$( $sandbox_flag   /usr/bin/wget     --user-agent="$useragent"  -qO-  "https://youtube.com/results?search_query="$xyz"&pbjreload=101&page=$xx" )"       > /tmp/.ytcache
+echo "$( $sandbox_flag  $scraper_flag  "$searchlink"$xyz"&pbjreload=101&page=$xx" )"       > /tmp/.ytcache
 parsing
 image_parsing
 page=$(cat /tmp/.ytname  | head -$xx | tail -1 )
 rage="$x&sp$page"
-echo $rage
 echo $($color2)"page no $(expr $xx + 1)$(tput sgr 0)"
-echo "URL: https://www.youtube.com/results?search_query="$rage" "
+echo "URL: $searchlink="$rage" "
 echo ""
 fi
 
+fi 
+####################### 
 
-
-fi # end of $p = n
-
-
-
+#############  search again ##############
 if  (([ "$p" != n ] && [ "$p" != q  ]) && [ "$mpv" != 1 ])
 then
     pqr=="`echo "$p" | sed 's/ /+/g'`"
-echo "$( $sandbox_flag   /usr/bin/wget  --user-agent="$useragent" -qO-  https://www.youtube.com/results?search_query="$pqr"&spfreeload=10)"      > /tmp/.ytcache
+echo "$( $sandbox_flag  $scraper_flag  "$searchlink"$pqr"&spfreeload=10")"      > /tmp/.ytcache
 rm -rf $imputdir
 
 #### imagescrape 
 #if [ $imageenable  -eq 1 ] ;  then mkdir $imputdir ; fi 
 cat /tmp/.ytcache | sed 's/thumb/\n/g'  | grep jpg | cut -c 17-64 | uniq -u | grep http   |sed 's/jpg.*/jpg/g' > "$imputfile"
-if [ $imageenable  -eq 1 ] ; then $sandbox_flag  /usr/bin/wget --user-agent="$useragent" -q   --input-file=$imputfile -P  $imputdir/  ;  fi 
-
+if [ $imageenable  -eq 1 ] ; then $sandbox_flag  $scraper_flag -q   --input-file=$imputfile -P  $imputdir/  ;  fi 
+#########
 
 x="$p"
 fi
 
-
+#######################
 
 mpv=0 # reset conflict var
-
 
 #clear
 done
 clear
 done
 clearing_cache
+      ;;
+esac
 
